b04ca9022d05e8e7532e264988ed262a1d4dd8df
AkshayM2001 <akshaymatale2001@gmail.com>
2024-03-15 03:44:07 +0530

    page source captured next get main target and store it or reflect on home page

--- home/webscrap-4.py
+++ home/webscrap-4.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from bs4 import BeautifulSoup
+import requests
 # from config import user,password
 
 # Set up Selenium WebDriver
@@ -13,7 +14,7 @@ url_data = "https://winwins.app/#/win"
 driver.get(url_login)
 
 # Wait for the page to load (you may need to adjust the wait time)
-time.sleep(5)  # Adjust the sleep time as needed
+time.sleep(3)  # Adjust the sleep time as needed
 
 # Find the username and password input fields using XPath
 username_field = driver.find_element(By.XPATH, "//input[@type='text']")
@@ -31,21 +32,30 @@ login_button.click()
 print('login succesfull')
 
 # Wait for the login process to complete (you may need to adjust the wait time)
-time.sleep(5)  # Adjust the sleep time as needed
+time.sleep(3)  # Adjust the sleep time as needed
 
 # Navigate to the data page
+# driver.get(url_data)
 driver.get(url_data)
 
 # Wait for the data page to load (you may need to adjust the wait time)
-time.sleep(5)  # Adjust the sleep time as needed
+# time.sleep(5)  # Adjust the sleep time as needed
 
 # Get the HTML content of the data page
+# html = driver.page_source
+
 html = driver.page_source
+
+time.sleep(5)
 
 # Use BeautifulSoup to parse the HTML content
 soup = BeautifulSoup(html, 'html.parser')
+print(soup.prettify())
 
 print('getting data')
 
+time.sleep(10)
+
 # Don't forget to close the browser when done
+
 driver.quit()
